04148b1abb07c86900d19cb39be0ed82c48ce7a4
Glicy <Ludovic2008@outlook.com>
2024-12-13 23:51:53 +0100

    better comments and all files

---
+++ iaQTable/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace iaQTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.target = new System.Windows.Forms.Panel();
+            this.sigma = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // target
+            // 
+            this.target.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.target.Location = new System.Drawing.Point(217, 199);
+            this.target.Name = "target";
+            this.target.Size = new System.Drawing.Size(135, 128);
+            this.target.TabIndex = 0;
+            // 
+            // sigma
+            // 
+            this.sigma.BackColor = System.Drawing.Color.Blue;
+            this.sigma.Location = new System.Drawing.Point(416, 408);
+            this.sigma.Name = "sigma";
+            this.sigma.Size = new System.Drawing.Size(74, 71);
+            this.sigma.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.sigma);
+            this.Controls.Add(this.target);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel target;
+        private System.Windows.Forms.Panel sigma;
+    }
+}
+
---
+++ iaQTable/sigma.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iaQTable
+{
+    public class Sigma
+    {
+        private int _mapSize;
+        private int _nbCases;
+        private Dictionary<(int, int), Dictionary<string, double>> _qTable = new Dictionary<(int, int), Dictionary<string, double>>();
+        private double _learningRate;
+        private double _discount;
+
+        /// <summary>
+        /// Constructor of Sigma
+        /// </summary>
+        /// <param name="mapSize">The size of one side of the map</param>
+        /// <param name="learningRate">If the bot will learn much or not (0-1)</param>
+        /// <param name="discount">If the bot will use future informations or not (0-1)</param>
+        public Sigma(int mapSize, double learningRate, double discount)
+        {
+            _mapSize = mapSize;
+            _learningRate = learningRate;
+            _discount = discount;
+            _nbCases = (int)Math.Pow(mapSize, 2);
+        }
+
+        /// <summary>
+        /// Initialize the QTable
+        /// </summary>
+        public void initQTable()
+        {
+            for (int x = 0; x < _mapSize; x++)
+            {
+                for (int y = 0; y < _mapSize; y++)
+                {
+                    // Put 0 points foreach action in the coordinate
+                    _qTable.Add((x, y), new Dictionary<string, double>{
+                        { "up", 0.0 },
+                        { "down", 0.0 },
+                        { "left", 0.0 },
+                        { "right", 0.0 }
+                    });
+                }
+            }
+        }
+    }
+}
